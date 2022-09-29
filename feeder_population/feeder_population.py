28704d10cc7d0a3e207fc07a682c2842e987966a
ebalogun01 <ebalinga2@gmail.com>
2022-09-29 22:01:55 +0000

    updated feeder population code to take in new file

--- feeder_population/feeder_population.py
+++ feeder_population/feeder_population.py
@@ -10,7 +10,7 @@ import ast
 import pickle
 
 #read config file
-os.chdir('/home/ec2-user/EV50_cosimulation/feeder_population')
+os.chdir('/home/ec2-user/EV50_cosimulation/feeder_population')  # change directory
 f=open('config.txt','r')
 param_dict=f.read()
 f.close()
