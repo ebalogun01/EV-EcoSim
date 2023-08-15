9474bb3c43e79d4e6adddb38d5d2b51766877100
ebalogun01 <42688418+ebalogun01@users.noreply.github.com>
2023-08-14 21:47:02 -0700

    Updating README

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 sys.path.append('../../EV50_cosimulation/charging_sim')
 
 project = 'EV-Ecosim'
-copyright = '2023, Emmanuel Balogun'
+copyright = '2023, Emmanuel Balogun, Stanford University S3L Lab'
 author = 'Emmanuel Balogun'
 release = '1.0.0'
 
