6667bb5956ba42f0d0b4889fbdec0a4183136a45
ebalogun01 <42688418+ebalogun01@users.noreply.github.com>
2023-08-30 15:06:47 -0700

    including docstring changes

--- test_cases/battery/gblvar.py
+++ test_cases/battery/gblvar.py
@@ -1,7 +1,12 @@
-"""This file defines some global variables used by the event_handlers.py file. This allows ease of variable sharing
+"""
+This module defines some global variables used by the event_handlers.py file. This allows ease of variable sharing
 between the relevant file
-Inputs: real_power.csv
-        reactive_power.csv"""
+
+Input files are:
+    * `real_power.csv`
+    * `reactive_power.csv`
+
+"""
 
 import numpy as np
 import pandas as pd
@@ -59,16 +64,21 @@ trans_dt = 10.0  # integration timestep [seconds]?
 trans_Ta = 20.0  # ambient temperature[C] {SLIGHTLY HIGHER THAN JUNE AVERAGE IN 2018}
 
 # TODO: find where all these transformer values were obtained from
-# transformer has various cooling modes that determine m and n for transformer
-# ONAF: Natural convection flow of oil through windings and radiators. Forced convection flow of air over radiators by fans
-# ONAN: Natural convection flow of oil through the windings and radiators. Natural convection flow of air over tank and radiation
+# Transformer has various cooling modes that determine m and n for transformer.
+
+# ONAF: Natural convection flow of oil through windings and radiators. Forced convection flow of air over radiators by
+# fans.
+
+# ONAN: Natural convection flow of oil through the windings and radiators. Natural convection flow of air over tank
+# and radiation.
+
 trans_R = 5.0   # ratio of copper loss to iron loss at rated load
 trans_tau_o = 2 * 60 * 60.0 # top oil time constant in seconds
 trans_tau_h = 6 * 60.0  # hotspot time constant in seconds
 trans_n = 0.9   # how did we get these numbers ?
 trans_m = 0.8   # how did we get these numbers ?
-trans_delta_theta_hs_rated = 28.0   # NEED TO DECIDE HOW IMPORTANT THESE WILL BE IN THE PAPER
-trans_delta_theta_oil_rated = 36.0  # todo: find good sources for these numbers
+trans_delta_theta_hs_rated = 28.0   # NEED TO DECIDE HOW IMPORTANT THESE WILL BE
+trans_delta_theta_oil_rated = 36.0  # todo: double-verify
 
 trans_To0 = 30.0  # initial oil temperature [C] (assume we start at a point where oil is slightly hotter than ambient)
 trans_Th0 = 60.0  # initial hot spot temperature [C]    # How is this set? (should not matter long-term)
