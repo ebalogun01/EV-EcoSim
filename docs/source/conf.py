c2bb7f250769644fdc33b009831fea0bf57746b0
ebalogun01 <42688418+ebalogun01@users.noreply.github.com>
2023-10-18 14:29:15 -0700

    adding latest webdoc changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ exclude_patterns = ['_build', 'Thumbs.db', 'DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-master_doc = "contents"
+master_doc = "index"
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_show_sourcelink = False
