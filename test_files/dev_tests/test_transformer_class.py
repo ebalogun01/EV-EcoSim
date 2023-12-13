95ee40d6fea1ad06e42995db8301ae1d9e19e3e3
ebalogun01 <42688418+ebalogun01@users.noreply.github.com>
2023-12-13 00:08:35 -0800

    updating paths to pass with linux os

--- test_files/dev_tests/test_transformer_class.py
+++ test_files/dev_tests/test_transformer_class.py
@@ -1,4 +1,6 @@
 import unittest
+import sys
+sys.path.append('../..')
 from charging_sim.transformer import OilTypeTransformer as Transformer
 from charging_sim.clock import Clock
 import pandas as pd
