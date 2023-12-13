291f258d3cd751124753c70c2aedf537d2da246e
ebalogun01 <42688418+ebalogun01@users.noreply.github.com>
2023-12-12 23:32:29 -0800

    updating test files and slight path corrections

--- test_files/dev_tests/test_batt_sys_identification.py
+++ test_files/dev_tests/test_batt_sys_identification.py
@@ -3,7 +3,7 @@ from batt_sys_identification.battery_identification import BatteryParams
 import pandas as pd
 
 
-data = pd.read_csv('../batt_sys_identification/batt_iden_test_data_W10_2.csv')
+data = pd.read_csv('../../batt_sys_identification/paper_data/batt_iden_test_data_W10_1.csv')
 
 
 class MyTestCase(unittest.TestCase):
@@ -18,14 +18,14 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(module is not None)
 
     def test_ga(self):
-        num_seq_runs = 4
+        num_seq_runs = 1
         module = BatteryParams(data)
         for i in range(num_seq_runs):
-            module.run_sys_identification(use_initial_pop=False)
-            module.plot_correction_scheme_comparison()
+            module.run_sys_identification(use_initial_pop=False, generations=2)
+            # module.plot_correction_scheme_comparison()
             self.assertEqual(len(module.params), 7)
             self.assertEqual(len(module.params_uncorr), 7)
-            module.plot_Ro()
+            # module.plot_Ro()
 
 
 if __name__ == '__main__':
